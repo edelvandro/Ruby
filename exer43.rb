7a5d3a08195ce85d6eff3e69918a519e57f25c1f
Edelvandro  Fernandes <edelvandro@gmail.com>
2021-04-23 11:54:35 -0300

    teste versionamento

---
+++ exer43.rb
@@ -0,0 +1,11 @@
+# Crie um programa que mostre na tela todos os números pares que estão no intervalo de 1 a 50.
+
+#não concluido, apenas tesdo parao gitlab
+puts "Digite um número entre 1 e 50: "
+num = gets.chomp.to_i
+
+
+if num % 2 == 0
+    puts num
+end
+
